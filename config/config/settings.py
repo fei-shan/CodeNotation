ef3ef12d3f2b940095fd7823adefd6f4a80ae36c
Fei Shan <fshan@terpmail.umd.edu>
2021-12-03 20:06:06 -0500

    heroku

--- config/config/settings.py
+++ config/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!lncni(u)&&u25-b$m541s2l=k0l#xt+1^(*9xq!w!umy8@6%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "codenotation.herokuapp.com"]
 
 
 # Application definition
